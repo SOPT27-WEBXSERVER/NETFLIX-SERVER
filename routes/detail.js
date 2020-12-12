5b1b8281d4295c3108f3c2aee7941d3f894563d8
GaYoung <kians2@naver.com>
2020-12-13 00:55:09 +0900

    상세 추천, 배우 완료

--- routes/detail.js
+++ routes/detail.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const detailController = require('../controllers/detail');
 
 router.get('/series/:idx', detailController.getSeries);
-//router.get('/recommend/:idx', detailController.createUser);
-//router.get('/actors/:idx', detailController.createUser);
+router.get('/recommend/:idx', detailController.getRecommendation);
+router.get('/actors/:idx', detailController.getActors);
 
 module.exports = router;
